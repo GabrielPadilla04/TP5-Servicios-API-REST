dfd393cbd0f54f9c2a37730c5293626d749bb957
Valentin Montes <Valentinmontes96@gmail.com>
2026-09-21 14:19:39 -0300

    Agregado de excepciones

---
+++ Excepciones/BaseDeDatosException.cs
@@ -0,0 +1,9 @@
+﻿namespace TP5_Servicios_API_REST.Excepciones
+{
+    public class BaseDeDatosException : Exception
+    {
+        public BaseDeDatosException(string mensaje) : base(mensaje)
+        {
+        }
+    }
+}
---
+++ Excepciones/DatosLlegaronErradosException.cs
@@ -0,0 +1,9 @@
+﻿namespace TP5_Servicios_API_REST.Excepciones
+{
+    public class DatosLlegaronErradosException : Exception
+    {
+        public DatosLlegaronErradosException(string mensaje) : base(mensaje)
+        {
+        }
+    }
+}
